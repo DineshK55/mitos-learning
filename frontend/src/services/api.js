5f2333be77d5bfb1276238545ea27457dc1a4726
DineshK55 <arasudinesh230@gmail.com>
2026-05-21 10:34:37 +0530

    Fixed production backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -11,7 +11,7 @@ import axios from "axios";
 const api = axios.create({
  //baseURL:"http://localhost:5000/api",
 
-baseURL: "https://mitos-learning.onrender.com/api",
+baseURL: "https://mitos-learning-backend.onrender.com/api",
 
   headers: {
     "Content-Type":
